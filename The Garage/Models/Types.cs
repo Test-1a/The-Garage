e95ecc98c6bab7c18d03d3d93cf74c2f571e24d0
Rod Paperfon <admin@paperfon.com>
2019-12-27 10:24:27 +0100

    Add project files.

---
+++ The Garage/Models/Types.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Garage.Models
+{
+    public class Types
+    {
+        public int Id { get; set; }
+        public string TypeOfVehicle { get; set; }
+        public Vehicles Vehicle { get; set; }
+    }
+}
---
+++ The Garage/Models/Vehicles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Garage.Models
+{
+    public class Vehicles
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You have to write a Registration Number")]
+        [Display(Name = "Registration Number")]
+        public string RegNr { get; set; }
+        
+        [Display(Name = "Start Time of Parking")]
+        public DateTime TimeOfParking { get; set; }
+
+        [Range(0, 99, ErrorMessage = "No more than 99 wheels")]
+        [Display(Name = "Number Of Wheels")]
+        public int NumnOfWheels { get; set; }
+
+        public string Color { get; set; }
+        [Display(Name = "Model of Vehicle")]
+        public string Model { get; set; }
+        [Display(Name = "Brand of Vehicle")]
+        public string Brand { get; set; }
+
+        public int TypeId { get; set; }
+        public int MemberId { get; set; }
+
+        public Types Type { get; set; }
+        public Members Member { get; set; }
+    }
+}
