cb37117255f86049c2fd5fcd26492ddcc30ff8e6
Rod Paperfon <admin@paperfon.com>
2020-01-03 10:24:43 +0100

    More layout and css changes

--- The Garage/Models/Members.cs
+++ The Garage/Models/Members.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace The_Garage.Models
     public class Members
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         public ICollection<Vehicles> Vehicle { get; set; }
--- The Garage/Models/Types.cs
+++ The Garage/Models/Types.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace The_Garage.Models
     public class Types
     {
         public int Id { get; set; }
+
+        [Display(Name = "Type of Vehicle")]
         public string TypeOfVehicle { get; set; }
 
         public ICollection<Vehicles> Vehicle { get; set; }
