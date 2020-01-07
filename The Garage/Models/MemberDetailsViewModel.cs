ebf1f67ffed4f18bb4013d19b5d3085768a09f7b
Rod Paperfon <admin@paperfon.com>
2020-01-07 10:49:19 +0100

    Changes in detailsviewmodel and css

--- The Garage/Models/MemberDetailsViewModel.cs
+++ The Garage/Models/MemberDetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace The_Garage.Models
     public class MemberDetailsViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Number of Vehicles")]
         public int NumberOfVehicles { get; set; }
         public IEnumerable<Vehicles> TheVehicles { get; set; }
     }
